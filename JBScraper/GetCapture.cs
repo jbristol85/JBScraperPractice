7c3ed1e86f6744f46bad4b572a7aa9ed9848693e
jbristol85 <josh.bristol2@gmail.com>
2018-06-27 00:27:31 -0400

    Adjusting data types

--- JBScraper/GetCapture.cs
+++ JBScraper/GetCapture.cs
@@ -20,33 +20,22 @@ namespace JBScraper
 
 //            var options = new ChromeOptions();
 //            options.AddArgument("--disable-gpu");
+
+
             // creates new chromeDriver and goes to webpage
             var chromeDriver = new ChromeDriver(@"./");
             chromeDriver.Navigate().GoToUrl("https://login.yahoo.com/");
-            // waits for page to load
-            //            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(14);
-            // login sequence
-            //            chromeDriver.FindElementById("uh-signedin").Click();
             chromeDriver.FindElementById("login-username").Click();
-//            chromeDriver.Keyboard.SendKeys(username);
             chromeDriver.Keyboard.SendKeys(usernamePassword.YahooUsername());
             chromeDriver.Keyboard.SendKeys(Keys.Enter);
             chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             chromeDriver.FindElementById("login-passwd").Click();
-//            chromeDriver.Keyboard.SendKeys(password1);
             chromeDriver.Keyboard.SendKeys(usernamePassword.YahooPassword());
             chromeDriver.Keyboard.SendKeys(Keys.Enter);
-            //navigate to portfolio with stocks
-            //            chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolios?bypass=true");
-
-            //            chromeDriver.FindElementById("__dialog").Click();
-            //            chromeDriver.Keyboard.SendKeys(Keys.Escape);
-            //navigate to Jbristol Portfolio
             chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v2");
             chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             chromeDriver.FindElement(By.Id("__dialog")).Click();
             chromeDriver.Keyboard.SendKeys(Keys.Escape);
-            // get currentValue dayGains and totalGain
             var currentValue = chromeDriver
                 .FindElementByXPath("//*[@id=\"main\"]/section/header/div/div[1]/div/div[2]/p[1]").Text;
             var dayGain = chromeDriver
@@ -58,22 +47,14 @@ namespace JBScraper
             capture.CaptureDate = DateTime.Now;
             capture.PortfolioValue = double.Parse(currentValue, NumberStyles.Currency);
             capture.DayGain = double.Parse(dayGain[0]);
-            //            capture.PercentDayGain = double.Parse(dayGainPercent.Remove(0, 1).Remove(dayGainPercent.Length - 2, dayGainPercent.Length - 1));
             capture.PercentDayGain = (double.Parse(dayGainPercent.Trim(new char[] { ' ', '(', '%', ')' }))/100);
             capture.TotalGain = double.Parse(totalGain[0]);
-            //            capture.PercentTotalGain = double.Parse(totalGainPercent.Remove(0, 1).Remove(dayGainPercent.Length - 2, dayGainPercent.Length - 1));
             capture.PercentTotalGain = (double.Parse(totalGainPercent.Trim(new char[] { ' ', '(', '%', ')' }))/100);
-//            Console.WriteLine(
-//                "currentValue: {0}, dayGain: {1}, totalGain: {2}, dayGainPercent: {3}, totalGainPercent: {4}",
-//                PortfolioValue, DayGain, TotalGain, PercentDayGain, PercentTotalGain);
 
-            //            // scrape symbols
+            //  Capture StockInfo
             var portfolioStockInfo = new List<StockInfo>();
-            //            var table = chromeDriver.FindElement(By.TagName("table"));
             var table = chromeDriver.FindElement(By.ClassName("tJDbU"));
-            //            Console.WriteLine("Table" + table);
-            //            var tableRows = table.FindElements(By.TagName("tr"));
-            var tableRows = table.FindElements(By.ClassName("_14MJo"));
+           var tableRows = table.FindElements(By.ClassName("_14MJo"));
             Console.WriteLine("tablerows" + tableRows.Count);
             var listRowData = new List<string>();
 
@@ -126,7 +107,6 @@ namespace JBScraper
 
             }
             chromeDriver.Quit();
-//            Console.WriteLine("portfolioStockInfo:   " + portfolioStockInfo);
             capture.StockInfo = portfolioStockInfo;
             return capture;
         }
--- JBScraper/Models/StockInfo.cs
+++ JBScraper/Models/StockInfo.cs
@@ -12,45 +12,45 @@ namespace JBScraper.Models
         public string StockSymbol { get; set; }
 
         [Display(Name = "Current Price")]
-//        [DataType(DataType.Currency)]
-        public string StockCurrentPrice { get; set; }
+        [DataType(DataType.Currency)]
+        public double StockCurrentPrice { get; set; }
 
         [Display(Name = "Price Change")]
-//        [DataType(DataType.Currency)]
-        public string StockPriceChange { get; set; }
+        [DataType(DataType.Currency)]
+        public double StockPriceChange { get; set; }
 
         [Display(Name = "Price Change Percent")]
-//        [DisplayFormat(DataFormatString = "{0:P2}")]
-        public string StockPriceChangePercent { get; set; }
+        [DisplayFormat(DataFormatString = "{0:P2}")]
+        public double StockPriceChangePercent { get; set; }
 
         [Display(Name = "Shares")]
-        public string StockShares { get; set; }
+        public double StockShares { get; set; }
 
         [Display(Name = "Cost Basis")]
-//        [DataType(DataType.Currency)]
-        public string StockCostBasis { get; set; }
+        [DataType(DataType.Currency)]
+        public double StockCostBasis { get; set; }
 
         [Display(Name = "Market Value")]
-//        [DataType(DataType.Currency)]
-        public string StockMarketValue { get; set; }
+        [DataType(DataType.Currency)]
+        public double StockMarketValue { get; set; }
 
         [Display(Name = "Day Gain")]
-//        [DataType(DataType.Currency)]
-        public string StockDayGain { get; set; }
+        [DataType(DataType.Currency)]
+        public double StockDayGain { get; set; }
 
         [Display(Name = "Day Gain Percent")]
-//        [DisplayFormat(DataFormatString = "{0:P2}")]
-        public string StockDayGainPercent { get; set; }
+        [DisplayFormat(DataFormatString = "{0:P2}")]
+        public double StockDayGainPercent { get; set; }
 
         [Display(Name = "Total Gain")]
-//        [DataType(DataType.Currency)]
-        public string StockTotalGain { get; set; }
+        [DataType(DataType.Currency)]
+        public double StockTotalGain { get; set; }
 
         [Display(Name = "Total Gain Percent")]
-//        [DisplayFormat(DataFormatString = "{0:P2}")]
-        public string StockTotalGainPercent { get; set; }
+        [DisplayFormat(DataFormatString = "{0:P2}")]
+        public double StockTotalGainPercent { get; set; }
 
-        public string StockLots { get; set; }
+        public Int32 StockLots { get; set; }
 
         public string StockNotes { get; set; }
 
